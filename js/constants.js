7ffb017c0104249fd2215b76614cb181349eefef
saurav7747 <izsaurav777@gmail.com>
2026-07-19 07:10:50 +0530

    Add game configuration and tile type constants

---
+++ js/constants.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    GAME_WIDTH: 800,
+    GAME_HEIGHT: 450,
+    FPS: 60,
+    TILE_SIZE: 32,
+    GRAVITY: 1200,
+    TERMINAL_VELOCITY: 800,
+    FRICTION: 0.85
+};
+
+const TILE_TYPES = {
+    EMPTY: 0,
+    SOLID: 1,
+    ONE_WAY: 2,
+    HAZARD: 3,
+    COIN: 4
+};
+
