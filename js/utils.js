8b5cc32e4f8a2f3d98360a49a1cdf350b2b5f07a
saurav7747 <izsaurav777@gmail.com>
2026-07-19 07:24:17 +0530

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,9 +1,44 @@
-const Utils = {
-    AABB: (r1, r2) => {
-        return r1.x < r2.x + r2.w && r1.x + r1.w > r2.x &&
-               r1.y < r2.y + r2.h && r1.y + r1.h > r2.y;
-    },
-    clamp: (val, min, max) => Math.max(min, Math.min(max, val)),
-    lerp: (a, b, t) => a + (b - a) * t,
-    rand: (min, max) => Math.random() * (max - min) + min
-};
+export class MathUtils {
+    static clamp(val, min, max) {
+        return Math.min(Math.max(val, min), max);
+    }
+
+    static lerp(start, end, amt) {
+        return (1 - amt) * start + amt * end;
+    }
+
+    static aabbTest(rect1, rect2) {
+        return rect1.x < rect2.x + rect2.width &&
+               rect1.x + rect1.width > rect2.x &&
+               rect1.y < rect2.y + rect2.height &&
+               rect1.y + rect1.height > rect2.y;
+    }
+
+    static getDistance(x1, y1, x2, y2) {
+        const dx = x2 - x1;
+        const dy = y2 - y1;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
+
+export class PubSub {
+    constructor() {
+        this.topics = {};
+    }
+
+    subscribe(topic, listener) {
+        if (!this.topics[topic]) this.topics[topic] = [];
+        this.topics[topic].push(listener);
+        return () => {
+            this.topics[topic] = this.topics[topic].filter(l => l !== listener);
+        };
+    }
+
+    publish(topic, data) {
+        if (!this.topics[topic]) return;
+        for (let i = 0; i < this.topics[topic].length; i++) {
+            this.topics[topic][i](data);
+        }
+    }
+}
+
