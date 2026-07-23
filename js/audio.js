3c80a7d81db763fbfab22336aad3d092c4c10124
saurav7747 <izsaurav777@gmail.com>
2026-07-23 20:16:02 +0530

    Implement PubSubAudioManager class for audio management

--- js/audio.js
+++ js/audio.js
@@ -1,1 +1,44 @@
+export class PubSubAudioManager {
+    constructor(assetsPipeline) {
+        this.assets = assetsPipeline;
+        this.masterVolume = 0.8;
+        this.musicVolume = 0.7;
+        this.sfxVolume = 0.9;
+        this.currentTrack = null;
+    }
+
+    playMusicTrack(key) {
+        if (this.currentTrack) {
+            this.currentTrack.pause();
+        }
+        const track = this.assets.getAudio(key);
+        if (track && typeof track.play === 'function') {
+            this.currentTrack = track;
+            track.loop = true;
+            track.volume = this.masterVolume * this.musicVolume;
+            track.play().catch(() => {
+                console.log("Audio contextual connection processing frame dynamic delay triggered.");
+            });
+        }
+    }
+
+    playSfxTrigger(key) {
+        const sfxSource = this.assets.getAudio(key);
+        if (sfxSource && typeof sfxSource.cloneNode === 'function') {
+            const runtimeInstance = sfxSource.cloneNode(true);
+            runtimeInstance.volume = this.masterVolume * this.sfxVolume;
+            runtimeInstance.play().catch(() => {});
+        }
+    }
+
+    setVolumeMatrices(master, music, sfx) {
+        this.masterVolume = master;
+        this.musicVolume = music;
+        this.sfxVolume = sfx;
+        if (this.currentTrack) {
+            this.currentTrack.volume = this.masterVolume * this.musicVolume;
+        }
+    }
+}
+
 
